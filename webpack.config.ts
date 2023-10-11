ce6e7d39be9fa5973360bd1de2e48ac4d344201a
Waren Gador <waren.gador15@gmail.com>
2023-10-11 19:09:29 +0800

    update ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,23 @@
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const CopyWebpack = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
 const path = require('path');
+
 module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'assets/[name].[contenthash].js',
-    // publicPath: '/' // for serverside and react-router-dom
   },
   target: 'web',
   devServer: {
     port: 4600,
     proxy: {
       '/api': {
-        target: 'http://localhost:8800', // Proxy Origin 
+        target: 'http://localhost:8800',
         secure: false,
         changeOrigin: true,
       },
@@ -39,7 +39,11 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'tsx',
+              target: 'es2015',
+            },
           },
         ],
       },
@@ -55,14 +59,14 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name].[contenthash][ext]', // Output path for images
+          filename: 'images/[name].[contenthash][ext]',
         },
       },
       {
         test: /\.(mp4|webm|ogg|ogv)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'videos/[name].[contenthash][ext]', // Output path for videos
+          filename: 'videos/[name].[contenthash][ext]',
         },
       },
     ],
@@ -70,12 +74,12 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [
-      new CssMinimizerPlugin(), // Minimize CSS
-      new TerserPlugin(), // Minimize JavaScript
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
     ],
     splitChunks: {
       chunks: 'async',
-      minSize: 2000, // Adjust this value as needed, e.g., 20000 bytes
+      minSize: 2000,
       maxSize: 20000,
       minChunks: 1,
       maxAsyncRequests: 30,
@@ -93,22 +97,22 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, './', 'index.html'),
+      template: path.join(__dirname, 'index.html'),
     }),
     new webpack.ProvidePlugin({
       React: 'react',
       ReactDOM: 'react-dom',
       'ReactRouterDOM': 'react-router-dom',
     }),
-    new CopyWebpack({
+    new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/images', // image directory origin
-          to: 'images', // image directory destination
+          from: 'src/images',
+          to: 'images',
         },
         {
-          from: 'src/videos', // videos directory origin
-          to: 'videos', // videos directory destination
+          from: 'src/videos',
+          to: 'videos',
         },
       ],
     }),
@@ -116,5 +120,3 @@ module.exports = {
   ],
   devtool: 'source-map',
 };
-
-// You should not alter this webpack.ts configuration unless you are a tester.
